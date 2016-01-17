1130eeddd098143eed8298e5d8b443eb72823778
BitMasterZ <adlakhashobhit@gmail.com>
2016-01-16 21:31:20 -0500

    updated stuff

---
+++ PjGam2/PajamaJam/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerStats : MonoBehaviour {
+
+	//public float Health = 1f;
+	public Slider Health;
+	public Slider WarpCD;
+	public Slider TimeCD;
+	public Rigidbody WarpProj;
+	PlayerMovement mv;
+	float Wspeed;
+	float Bdamage = 0.3f;
+	float Rdamage = 0.5f;
+	float Ldamage = 1.0f;
+	float Adamage = 0.1f;
+	// Use this for initialization
+	void Start () {
+
+		WarpCD.value = 0f;
+		TimeCD.value = 0f;
+		Wspeed = 20f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (WarpCD.value > 0f) {
+			WarpCD.value -= (Time.deltaTime/3f);
+		}
+		if (TimeCD.value > 0f) {
+			TimeCD.value -= (Time.deltaTime/10f);
+		}
+	}
+
+	void OnCollisionEnter(Collision coll){
+		mv = coll.gameObject.GetComponent<PlayerMovement> ();
+		//print ("hit");
+		switch (coll.gameObject.tag) {
+		case "Bullet": 
+			Health.value -= Bdamage;
+			break;
+		case "Rocket":
+			Health.value -= Rdamage;
+			break;
+		default:
+			Health.value -= Adamage;
+			break;
+		}
+
+		if(Health.value <= 0){
+			print ("gg");
+			//trigger Game Over
+		}
+		//print (Health.value*100);
+	}
+
+	public void SlowProj(){
+		if (TimeCD.value <= 0) {
+			PlayerPrefs.SetFloat ("Speed", 0.1f);
+			Invoke ("SpeedUp", 3f);
+		}
+	}
+	
+	void SpeedUp(){
+		TimeCD.value = 1f;
+		PlayerPrefs.SetFloat("Speed", 1f);
+	}
+
+	public void Warp(){
+		if (WarpCD.value <= 0) {
+			Rigidbody proj = (Rigidbody)Instantiate (WarpProj, this.transform.TransformPoint (Vector3.right * 5), WarpProj.gameObject.transform.rotation);
+			proj.velocity = this.transform.TransformDirection (Vector3.right * Wspeed* PlayerPrefs.GetFloat ("Speed"));
+			WarpCD.value = 1f;
+		}
+	}
+}
---
+++ PjGam2/PajamaJam/Assets/Scripts/ProjController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjController : MonoBehaviour {
+	//public float Damage;
+	// Use this for initialization
+	Vector3 vel;
+	public GameObject explosion;
+
+	void Start () {
+		vel = this.gameObject.GetComponent<Rigidbody>().velocity;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.gameObject.GetComponent<Rigidbody> ().velocity = vel * PlayerPrefs.GetFloat ("Speed");
+	}
+
+	void OnCollisionEnter(Collision other){
+		//if (this.gameObject.tag == "Hunter") {
+		//	return;
+		//}
+		if (other.gameObject.tag != "Border" && other.gameObject.tag != "Hunter" && other.gameObject.tag!="Runner") {
+				Destroy (other.gameObject);
+				Destroy (this.gameObject);
+		}
+		Instantiate (explosion,this.transform.position, Quaternion.identity);
+		Destroy (this.gameObject);
+
+	}
+}
